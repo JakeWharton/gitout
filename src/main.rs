0f455dfb98d5bcc51c9d047c730449046c9c38bb
Jake Wharton <jakew@google.com>
2020-03-27 22:52:06 -0400

    Clone from GitHub list too

--- src/main.rs
+++ src/main.rs
@@ -29,22 +29,34 @@ fn main() {
     dbg!(&config);
   }
 
+  if let Some(github) = config.github {
+    let mut github_dir = destination.clone();
+    github_dir.push("github");
+
+    for repo in github.clone.repos {
+      // TODO switch to https and authenticated clones
+      let url = format!("git://github.com/{0}.git", &repo);
+      let path = format!("{0}/git", &repo);
+      clone_or_fetch_bare(&github_dir, &path, &url);
+    }
+  }
+
   if let Some(git) = config.git {
     let mut git_dir = destination.clone();
     git_dir.push("git");
 
     for (path, url) in git.repos {
       let url = url.as_str().unwrap();
-      clone_or_fetch_bare(&git_dir, path, url)
+      clone_or_fetch_bare(&git_dir, &path, url)
     }
   }
 }
 
-fn clone_or_fetch_bare(dir: &PathBuf, path: String, url: &str) {
+fn clone_or_fetch_bare(dir: &PathBuf, path: &str, url: &str) {
   let mut repo_dir = dir.clone();
   repo_dir.push(path);
 
-  if fs::metadata(&repo_dir).unwrap().is_dir() {
+  if fs::metadata(&repo_dir).map_or_else(|_| false, |m| m.is_dir()) {
     let repository = Repository::open_bare(&repo_dir).unwrap();
     let mut origin = repository.find_remote("origin").unwrap();
     origin.fetch(&[] as &[String], None, None).unwrap();
